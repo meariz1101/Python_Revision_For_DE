e3547262c176dd40a049a60e11136295a33bc537
meariz1101 <meariz1101@gmail.com>
2025-12-17 22:18:51 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,10 @@
+age=int(input('enter your age:'))
+if(age<13):
+    print('you are a child')
+elif(age<18):
+    print('you are a teenager')
+elif(age<64):
+    print('you are an adult')
+else:
+    print('you are a senior citizen')
+
